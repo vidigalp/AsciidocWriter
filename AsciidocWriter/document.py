e7a1f30a62e91dd5bf38fad9a7ba834378be3149
Pedro Vidigal <pedro.vidigal@instaclustr.com>
2021-12-15 14:27:14 +0000

    Added Header Template

--- AsciidocWriter/document.py
+++ AsciidocWriter/document.py
@@ -13,11 +13,25 @@ class Document():
 
     """
     def __init__(self, name:str, author:str, description:str):
+        """
+
+        @param name:
+        @param author:
+        @param description:
+        """
         self.name = name
         self.author = author
         self.description = description
         self.sections = [Section]
 
 
+    def set_document_title(self):
+        pass
+
+
     def add_section(self, section:Section):
+        """
+
+        @param section:
+        """
         self.sections.append(section)
