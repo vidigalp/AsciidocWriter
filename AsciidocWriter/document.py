21abfc344935b388c69703b20bc24b501b5a654f
Pedro Vidigal <pedro.vidigal@instaclustr.com>
2021-12-17 11:31:53 +0000

    Added Header Template

--- AsciidocWriter/document.py
+++ AsciidocWriter/document.py
@@ -4,15 +4,17 @@ This module implements the class that deals with the full document.
     :copyright: (c) 2021 by Pedro Vidigal.
     :license: MIT, see License for more details.
 """
+import uuid
+from pathlib import Path
 from typing import List
-from loguru import logger
-from .section import Section
-import yaml
-from pathlib import Path
-import jinja2
-from pathlib import Path
 
-ROOT_DIR = Path.cwd().parent
+import yaml
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+
+from .section import Section
+
+ROOT_DIR = Path(__file__).resolve().parent
 
 class Document():
     """
@@ -42,19 +44,20 @@ class Document():
                 "background_image_width": self.config['header']['title-page-image']['width'],
                 "background_image_height": self.config['header']['title-page-image']['height'],
                 "fit": self.config['header']['title-page-image']['fit'],
-                "opacity": self.config['header']['title-page-image']['opacity']}
+                "xrefstyle": "short",
+                "opacity": self.config['header']['title-page-image']['opacity'],
+                "doc_uuid": str(uuid.uuid1())}
 
         self.set_document_header(data=data)
 
     def set_document_header(self, data, template_path:Path=None):
-        if not template_path:
-            template_path = Path.joinpath(ROOT_DIR, 'templates')
-        templateLoader = jinja2.FileSystemLoader(searchpath=str(template_path))
-
-        templateEnv = jinja2.Environment(loader=templateLoader)
+        path = Path.joinpath(Path(__file__).resolve().parent, 'templates')
+        templateEnv = Environment(loader=FileSystemLoader(searchpath=path))
         template = templateEnv.get_template("health_check_header.jinja2").render()
 
         self.header = template.render(data)
+
+        i =2
 
 
 
